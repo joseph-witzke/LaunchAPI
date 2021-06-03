43601afa030ab0f70a601145e748c6bae68ca86d
Joe Witzke <joewitzke@joes-mbp.lan>
2021-06-03 14:02:08 -0600

    added status 404 catch

--- index.js
+++ index.js
@@ -14,6 +14,10 @@ server.get('/api', (req, res) => {
     res.json({ message: `${process.env.COHORT} ROCKS`})
 })
 
+server.use((req, res) => {
+    res.status(404).json({ message: 'not found' })
+})
+
 server.listen(PORT, () => {
     console.log(`listening on ${PORT}`)
 })
