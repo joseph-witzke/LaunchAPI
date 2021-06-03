ba9b1f2b4ba7baa1870ad31fe9e0781896d99cac
Joe Witzke <joewitzke@joes-mbp.lan>
2021-06-03 13:40:30 -0600

    env file created and server running

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("hey")
+require('dotenv').config()
+const express = require('express')
+const server = express()
+
+if (process.env.NODE_INV === 'production') {
+    console.log('this means this code is deployed')
+}
+
+const PORT = process.env.PORT || 5000
+
+console.log('port is -->', PORT)
+
+server.get('/api', (req, res) => {
+    res.json({ message: `${process.env.COHORT} ROCKS`})
+})
+
+server.listen(PORT, () => {
+    console.log(`listening on ${PORT}`)
+})
